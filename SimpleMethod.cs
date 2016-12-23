79879b1e7aede22459fa6a6fd33564f1030844f8
planetarianZero <m18333586810@163.com>
2016-12-23 15:26:46 +0800

    Create SimpleMethod.cs

---
+++ SimpleMethod.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game");
+            
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("In what city were you born?");
+            string city = Console.ReadLine();
+            
+            DisplayResult(ReverseString(firstName), ReverseString(lastName), ReverseString(city));
+
+            //Console.WriteLine();
+
+            //DisplayResult(ReverseString(firstName)+" "+ReverseString(lastName)+" "+ReverseString(city));
+
+            Console.ReadLine();
+
+        }
+
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            /*
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+            Console.Write(" ");
+            */
+            return String.Concat(messageArray);
+        }
+
+        private static void DisplayResult(string reverseFirstName, string reverseLastName, string reverseCity)
+        {
+            Console.Write("Results: ");
+            Console.WriteLine(String.Format("{0} {1} {2}", reverseFirstName, reverseLastName, reverseCity));
+        }
+        /*
+        private static void DisplayResult(string message)
+        {
+            Console.Write("Results: ");
+            Console.Write(message);
+        }
+        */
+
+    }
+}
